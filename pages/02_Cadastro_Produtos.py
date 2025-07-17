17aae8b4e2829fd79b05680aa67e4738f97197e3
Jeferson Souza <mp_bra_nco@hotmail.com>
2025-07-17 18:19:56 -0300

    Update 02_Cadastro_Produtos.py
    Criando controles de edição do banco

--- pages/02_Cadastro_Produtos.py
+++ pages/02_Cadastro_Produtos.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sqlite3
 
+from numpy.matlib import empty
+from sympy.codegen.ast import none
+
 from utils.config_cadastro import tratamento_cadastro, importar_cadastro
 from db.config_db import LOCAL_BANCO
 
@@ -33,6 +36,28 @@ with st.expander('Importar relatório', expanded=False):
     arquivo_cadastro = st.file_uploader("--", type=["xlsx", "xls"])
     df_cadastro = tratamento_cadastro(arquivo_cadastro)
     importar_cadastro(df_cadastro, 'cadastro_material')
+
+
+with st.expander('Edição cadastro', expanded=True):
+    cad_col01, cad_col02, cad_col03 = st.columns(3)
+
+    with cad_col01:
+        cod_produto = st.text_input('Código produto: ')
+        cod_grupo = st.text_input('Código de grupo:')
+        descricao = st.text_input('Descrição material', key='descr_01')
+        #descricao_detalhada = st.text_input('Descrição material', key='descr_02')
+
+
+    with cad_col02:
+        unidade_med = st.text_input('Unidade Medida')
+        peso = st.number_input('Peso liquido:', min_value=0.0, max_value=100.0)
+        peso_bruto = st.number_input('Peso Bruto:', min_value=0.0, max_value=100.0)
+
+    with cad_col03:
+        qtd_embalagem = st.number_input('Quantidade bobinas no fardo: ', min_value=0, max_value=10)
+        peso_fardo = float(peso_bruto) * int(qtd_embalagem)
+        st.text(f"Peso de Fardo = {peso_fardo}")
+        #st.text_input('Peso do fardo', value=peso_fardo)
 
 
 with st.expander("Cadastro de Material", expanded=True):
